8d6f85862e14eb0a7dae35ee26290a9d8541392d
Acquati <leandroacquati@gmail.com>
2022-05-16 06:29:01 -0300

    better message handling

--- public/index.js
+++ public/index.js
@@ -3,24 +3,24 @@ let peerID
 let receiverPeerID
 let receiverConnection
 let senderConnection
-let isConnectionCreator = false
+let isSendingConnection = false
 const videoNode = document.getElementById('videoNode')
+
+const renderMessage = (message) => {
+  const messageTag = document.createElement('p')
+  const messageText = document.createTextNode(message)
+  messageTag.appendChild(messageText)
+  const receivedMessagesNode = document.getElementById('receivedMessagesNode')
+  receivedMessagesNode.appendChild(messageTag)
+}
 
 const renderVideo = (stream) => {
   videoNode.srcObject = stream
 }
 
-const renderMessage = (message) => {
-  const tag = document.createElement('p')
-  const text = document.createTextNode(message)
-  tag.appendChild(text)
-  const element = document.getElementById('receivedMessages')
-  element.appendChild(tag)
-}
-
 peer.on('open', (id) => {
-  console.log('My peer ID is: ' + id)
   peerID = id
+  console.log('My peer ID is: ' + peerID)
   const peerIdNode = document.getElementById('peerIdNode')
   peerIdNode.innerText = peerID
 })
@@ -34,16 +34,14 @@ peer.on('connection', (connection) => {
 
   senderConnection.on('open', () => {
     console.log('Sender connection open.')
-  })
 
-  // Receive messages
-  senderConnection.on('data', (data) => {
-    renderMessage(data)
-    console.log('Sender sent: ' + data)
-  })
+    senderConnection.on('data', (data) => {
+      renderMessage('Sender sent: ' + data)
+      console.log('Sender sent: ' + data)
+    })
 
-  // Send messages
-  senderConnection.send('Hello from ' + peerID)
+    senderConnection.send('Hello from ' + peerID)
+  })
 })
 
 peer.on('call', (call) => {
@@ -66,39 +64,38 @@ connectButton.onclick = () => {
 
   receiverConnection.on('open', () => {
     console.log('Receiver connection open.')
-  })
 
-  // Receive messages
-  receiverConnection.on('data', (data) => {
-    renderMessage('Receiver sent: ' + data)
-    console.log('Receiver sent:', data)
-  })
-
-  // Send messages
-  receiverConnection.send('Hello from ' + peerID)
-
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: true })
-    .then((stream) => {
-      const call = peer.call(receiverPeerID, stream)
-      call.on('stream', renderVideo)
-    })
-    .catch((err) => {
-      console.log('Failed to get local stream', err)
+    receiverConnection.on('data', (data) => {
+      renderMessage('Receiver sent: ' + data)
+      console.log('Receiver sent:', data)
     })
 
-  isConnectionCreator = true
+    receiverConnection.send('Hello from ' + peerID)
+
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        const call = peer.call(receiverPeerID, stream)
+        call.on('stream', renderVideo)
+      })
+      .catch((err) => {
+        console.log('Failed to get local stream', err)
+      })
+
+    isSendingConnection = true
+  })
 }
 
 const sendMessageButton = document.getElementById('sendMessageButton')
 sendMessageButton.onclick = () => {
   const message = document.getElementById('messageInput').value
-  console.log('Sended: ' + message)
-  if (isConnectionCreator) {
+  if (isSendingConnection) {
+    renderMessage('Sender sent: ' + message)
+    console.log('Sender sent: ' + message)
     receiverConnection.send(message)
   } else {
+    renderMessage('Receiver sent: ' + message)
+    console.log('Receiver sent: ' + message)
     senderConnection.send(message)
   }
 }
-
-// const receivedMessages = document.getElementById('receivedMessages')
